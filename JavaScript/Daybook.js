3f5b4c9a7e774b91f0e260117ebaa9297a99aad3
vanshverma67 <vanshrakesh67@gmail.com>
2025-12-22 14:13:11 +0530

    improved daybook desgin and layout

--- JavaScript/Daybook.js
+++ JavaScript/Daybook.js
@@ -1,6 +1,34 @@
 document.getElementById('btnDaybook').addEventListener('click',()=>{
  let mainDiv = document.getElementById('main');
  mainDiv.innerHTML =`
-<div class="anme">vansh</div>
+<div class="bokhead">
+                <h2>Day Book</h2>
+                <div class="bookdate">
+                    <p>From</p>
+                    <input type="date">
+                    <p>to</p>
+                    <input type="date">
+                </div>
+            </div>
+           
+            <div class="booktitle">
+                <p>Date</p>
+                <p>particular</p>
+                <select  id="voucher">
+                    <option >voucher-types</option>
+                    <option>Contra</option>
+                    <option>Payment</option>
+                    <option>Recipt</option>
+                    <option>Journal</option>
+                    <option>Purchases</option>
+                    <option>Sales</option>
+                </select>
+                <p>cv-no</p>
+                <p>Inward CR</p>
+                <p>Inward DR</p>
+            </div>
+            <hr>
+            <div class="enterys">
+            </div>
  `
 });
